eaf6f21d70911f3f97f615004d840b6768d26e0c
utsukushii <utsukushii@earthlink.net>
2014-02-17 05:02:53 -0800

    Update project name.
    	project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/perl "0.0.1"
+(defproject com.utsukushii/LT-Perl "0.0.1"
   :description "Perl language plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
