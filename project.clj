2a338b283ace4f47e9cebc91f4afa1b05f8dc387
utsukushii <utsukushii@earthlink.net>
2014-02-17 05:55:04 -0800

    Bump version.
    	README.md
    	plugin.json
    	project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.utsukushii/LT-Perl "0.0.2"
+(defproject com.utsukushii/LT-Perl "0.0.3"
   :description "Perl language plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
